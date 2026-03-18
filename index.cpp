3c6df4dbeaf17ef92f11b1e47e8ab05f4c5fa54a
Tunar <tunarm2@icloud.com>
2026-03-18 18:58:00 +0200

    start

---
+++ index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void line(char symb,int count){
+    for(int i=0;i<count;++i){
+        cout<<symb;
+    }
+     cout<<endl;
+}
+int main(){
+
+
+    line('+', 20);
+    line('@', 5);
+    return 0;
+}
