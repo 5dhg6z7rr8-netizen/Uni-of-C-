5188f5b0edd16c531b0d42c2bde7d19eb929724e
Tunar <tunarm2@icloud.com>
2026-03-21 23:27:50 +0200

    added 2 functions and in function there are 2 ways of solving task

--- index.cpp
+++ index.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 void line(char symb,int count){
     for(int i=0;i<count;++i){
         cout<<symb;
@@ -7,19 +8,65 @@ void line(char symb,int count){
      cout<<endl; 
 }
 
+void foundsymb(string line, char num){
+    int founded = 0;
+    for(char i: line){
+        if(num==i){
+            founded++;
+        }
+    }
+    if(founded==0){
+        cout<<"No symbol";
+    }
+    else cout<<founded<< endl;
+}
+
+void del(string line, char num);
 
 int main(){
     
-    line('+', 20);
-    line('@', 5);
-    for(int i =0;i<5;++i){
-        cout<<1;
-    }
-    return 0;
-   
-    
-    
+    // line('+', 20);
+    // line('@', 5);
+    // for(int i =0;i<5;++i){
+    //     cout<<1;}
+
+    string line;
+    char num;
+
+    cout<<"Enter line: ";
+    getline(cin, line);
+    cout<<"Enter symb: ";
+    cin>> num;
+
+    // foundsymb(line, num);
+    del(line, num);
+
+
+
+
 }
+    
+void del(string line, char num){
+    // 1 way
+    // string res = "";
+    // for(char i: line){
+    //     if(num!=i){
+    //         res += i;
+    //     }
+    // }
+    // cout<< res<<endl;
+
+    // 2 way 
+    line.erase(remove(line.begin(), line.end(), num), line.end());
+    cout<<line<<endl;
+
+}  
+
+    
+
+    
+    
+
 
 
 
